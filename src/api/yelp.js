f7bde6e13a3f0323db55a107ce7029edcc8c82a2
tsevim <tolgasevim@gmail.com>
2021-01-23 14:59:52 +0100

    w

---
+++ src/api/yelp.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+//https://www.yelp.com/developers/v3/manage_app 
+export default axios.create({
+  baseURL: 'https://api.yelp.com/v3/businesses'
+
+
+
+  wuDUNTPUXq5PSGGgPmOKtvpLaEgyALD8_DfoF99D4oTbpG10qA8Ui-zbiI8-dAVJSR8upQd9yGMkp8IewZikc6Ef0jO_q1H07kx37THihmitWzCpNiTN-7h-1gmTXXYx
+}):
