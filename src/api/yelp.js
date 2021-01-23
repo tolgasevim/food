9db08d5e2a7168c5f64bf562bf00e54b6ec4e5db
tolgasevim <tolgasevim@gmail.com>
2021-01-23 18:25:10 +0100

    qwe

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
-//https://www.yelp.com/developers/v3/manage_app 
+//https://www.yelp.com/developers/v3/manage_app
 export default axios.create({
-  baseURL: 'https://api.yelp.com/v3/businesses'
-
-
-
-  wuDUNTPUXq5PSGGgPmOKtvpLaEgyALD8_DfoF99D4oTbpG10qA8Ui-zbiI8-dAVJSR8upQd9yGMkp8IewZikc6Ef0jO_q1H07kx37THihmitWzCpNiTN-7h-1gmTXXYx
-}):
+  baseURL: 'https://api.yelp.com/v3/businesses',
+  headers: {
+    Authorization : 'Bearer wuDUNTPUXq5PSGGgPmOKtvpLaEgyALD8_DfoF99D4oTbpG10qA8Ui-zbiI8-dAVJSR8upQd9yGMkp8IewZikc6Ef0jO_q1H07kx37THihmitWzCpNiTN-7h-1gmTXXYx'
+  }
+});
